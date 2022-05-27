f350f643938cfae8c11846efc290b390905565ae
S. Aguilar-Valdez <sofia.aguilarv@alumnos.udg.mx>
2022-05-26 20:13:43 -0500

    [dev] built generate automaton function in DFA.py module --it works!

--- DFA.py
+++ DFA.py
@@ -15,4 +15,27 @@ def generate_automaton(sigma,
                             start_state,
                             final_state,
                             test_strings):
+    predictions = []
+    print("\nTransitions = ")
     print(transitions_df)
+    for word in test_strings:
+        print("\n<---------------")
+        print("Test word = "+word)
+        current_state = start_state[0] # str
+        for letter in word:
+            #print(type(letter)) # str
+            print("\nTransition's input letter = "+letter)
+            print("\nCurrent state = "+current_state)
+            print("\nPossible transitions = ")
+            transition_df = transitions_df.loc[(transitions_df['before'] == current_state) & (transitions_df['input'] == letter)]
+            print(transition_df)
+            next_state = transition_df['after'].values
+            next_state = str(next_state[0]) # str
+            current_state = next_state
+        if (current_state in final_state):
+            print("\n-> Word "+ word +" is part of the language.")
+            predictions.append(True)
+        else:
+            predictions.append(False)
+        print("---------------\>")
+    print(predictions)
